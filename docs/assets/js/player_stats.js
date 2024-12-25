652bb91deaec95b5b2301d55af515c0d7a46d56b
vRaj-Ajmera <98247409+vRaj-Ajmera@users.noreply.github.com>
2024-12-24 18:03:41 -0800

    added sultan speppy to main.js and player_stats.js arrays

--- docs/assets/js/player_stats.js
+++ docs/assets/js/player_stats.js
@@ -1,5 +1,5 @@
 function addProfilePictures() {
-    const players = ["Raj", "Azhan", "Sameer", "Zetaa", "Adi", "Dylan", "Parum", "EnderRobot", "Lynden", "Rusheel"];
+    const players = ["Raj", "Azhan", "Sameer", "Zetaa", "Adi", "Dylan", "Parum", "EnderRobot", "Lynden", "Rusheel", "SultanSpeppy"];
     const defaultImagePath = "assets/icons/default.png";
 
     // Loop through all <td> elements in the document
