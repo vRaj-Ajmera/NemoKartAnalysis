56816060bb7679f3510f2d62849742ca70e4b92e
vRaj-Ajmera <98247409+vRaj-Ajmera@users.noreply.github.com>
2024-12-30 01:38:42 -0800

    added race13 - race25 test cases

--- tests/OCR_test_cases.py
+++ tests/OCR_test_cases.py
@@ -58,6 +58,67 @@ test_cases = {
     "race12.png": [
         {'placement': 2, 'player_name': 'Azhan', 'race_time': '2:48.60'},
         {'placement': 4, 'player_name': 'Sameer', 'race_time': '2:50.16'}
+    ],
+    "race13.png": [
+        {'placement': 3, 'player_name': 'Azhan', 'race_time': '2:40.26'},
+        {'placement': 4, 'player_name': 'Sameer', 'race_time': '2:41.70'}
+    ],
+    "race14.png": [
+        {'placement': 1, 'player_name': 'Sameer', 'race_time': '2:02.78'}
+    ],
+    "race15.png": [
+        {'placement': 1, 'player_name': 'Raj', 'race_time': '1:51.84'},
+        {'placement': 2, 'player_name': 'Sameer', 'race_time': '1:52.72'},
+        {'placement': 3, 'player_name': 'Azhan', 'race_time': '1:53.94'}
+    ],
+    "race16.png": [
+        {'placement': 2, 'player_name': 'Sameer', 'race_time': '2:49.56'}
+    ],
+    "race17.png": [
+        {'placement': 1, 'player_name': 'Sameer', 'race_time': '1:52.20'}
+    ],
+    "race18.png": [
+        {'placement': 3, 'player_name': 'Sameer', 'race_time': '1:52.02'},
+        {'placement': 4, 'player_name': 'Raj', 'race_time': '1:52.38'},
+        {'placement': 5, 'player_name': 'Azhan', 'race_time': '1:55.16'}
+    ],
+    "race19.png": [
+        {'placement': 1, 'player_name': 'Sameer', 'race_time': '2:36.74'},
+        {'placement': 3, 'player_name': 'Azhan', 'race_time': '2:39.20'}
+    ],
+    "race20.png": [
+        {'placement': 2, 'player_name': 'Azhan', 'race_time': '2:50.76'},
+        {'placement': 3, 'player_name': 'Sameer', 'race_time': '2:53.56'}
+    ],
+    "race21.png": [
+        {'placement': 1, 'player_name': 'Sameer', 'race_time': '2:43.24'},
+        {'placement': 2, 'player_name': 'Raj', 'race_time': '2:46.12'}
+    ],
+    "race22.png": [
+        {'placement': 1, 'player_name': 'Azhan', 'race_time': '2:42.66'},
+        {'placement': 2, 'player_name': 'Adi', 'race_time': '2:44.16'},
+        {'placement': 3, 'player_name': 'Dylan', 'race_time': '2:45.22'},
+        {'placement': 4, 'player_name': 'EnderRobot', 'race_time': '2:46.10'}
+    ],
+    "race23.png": [
+        {'placement': 1, 'player_name': 'Adi', 'race_time': '2:14.24'},
+        {'placement': 2, 'player_name': 'Dylan', 'race_time': '2:14.84'},
+        {'placement': 3, 'player_name': 'Azhan', 'race_time': '2:15.66'},
+        {'placement': 5, 'player_name': 'Lynden', 'race_time': '2:17.16'},
+        {'placement': 6, 'player_name': 'EnderRobot', 'race_time': '2:19.08'}
+    ],
+    "race24.png": [
+        {'placement': 1, 'player_name': 'Azhan', 'race_time': '2:43.62'},
+        {'placement': 4, 'player_name': 'Adi', 'race_time': '2:45.76'},
+        {'placement': 5, 'player_name': 'EnderRobot', 'race_time': '2:52.02'}
+    ],
+    "race25.png": [
+        {'placement': 1, 'player_name': 'Azhan', 'race_time': '2:40.16'},
+        {'placement': 2, 'player_name': 'Raj', 'race_time': '2:40.44'},
+        {'placement': 3, 'player_name': 'Sameer', 'race_time': '2:44.04'},
+        {'placement': 4, 'player_name': 'Adi', 'race_time': '2:45.62'},
+        {'placement': 6, 'player_name': 'Dylan', 'race_time': '2:47.68'},
+        {'placement': 7, 'player_name': 'Parum', 'race_time': '2:50.30'}
     ]
     # Add more test cases as needed
 }
